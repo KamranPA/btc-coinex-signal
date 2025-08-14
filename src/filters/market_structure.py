68bd309be0718c97435f5e91cb566af7a437defd
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-14 14:05:47 +0330

    Delete src/filters directory

--- src/filters/market_structure.py
+++
@@ -1,9 +0,0 @@
-def check_structure(df):
-    l = df.iloc[-1]
-    p = df.iloc[-2]
-    pp = df.iloc[-3]
-
-    long = (p['low'] > pp['low']) and (l['close'] > p['high'])
-    short = (p['high'] < pp['high']) and (l['close'] < p['low'])
-
-    return {"long": long, "short": short}
--- src/filters/volume.py
+++
@@ -1,7 +0,0 @@
-def confirm_volume(df):
-    l = df.iloc[-1]
-    volume_avg = df['volume'].tail(20).mean()
-    return {
-        "long": l['volume'] > 1.5 * volume_avg,
-        "short": l['volume'] > 1.5 * volume_avg
-    }
