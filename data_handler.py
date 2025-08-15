2d8ee3cc1651c047cf3d56892d75a4a6e6f31943
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-15 22:28:37 +0330

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -1,20 +1,22 @@
 # data_handler.py
 import ccxt
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime
 
 def fetch_binance_data(symbol, timeframe, limit=100, start_date=None, end_date=None):
-    exchange = ccxt.binance({
+    # تغییر: استفاده از kucoin به جای binance
+    exchange = ccxt.kucoin({
         'options': {
             'adjustForTimezone': False
-        }
+        },
+        'enableRateLimit': True,  # مهم: رعایت محدودیت درخواست
     })
 
     # تبدیل تاریخ به میلی‌ثانیه
     def to_timestamp(date_str):
         return int(datetime.strptime(date_str, "%Y-%m-%d").timestamp() * 1000)
 
-    # تنظیم پارامترها
+    # تنظیم since و limit
     params = {
         'limit': limit,
         'since': to_timestamp(start_date) if start_date else None
