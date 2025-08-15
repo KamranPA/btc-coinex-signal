ae0c6f64564a59ccd2b43dfc451a3bd19d3d7fb2
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-15 20:45:20 +0330

    Update logger_config.py

--- logger_config.py
+++ logger_config.py
@@ -1,12 +1,18 @@
 # logger_config.py
 import logging
+import os
 from datetime import datetime
 
+# ایجاد پوشه logs اگر وجود نداشته باشد
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+# تنظیمات لاگ
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s | %(levelname)s | %(message)s',
     handlers=[
-        logging.FileHandler(f"logs/{datetime.now().strftime('%Y-%m-%d')}.log"),
+        logging.FileHandler(f"{log_dir}/{datetime.now().strftime('%Y-%m-%d')}.log"),
         logging.StreamHandler()
     ]
 )
