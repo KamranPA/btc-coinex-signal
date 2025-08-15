64c720304b8d3e9b04d621599e0a914ba802ac73
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-15 21:26:31 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-{
-  "SYMBOL": "BTC/USDT",
-  "TIMEFRAME": "15m",
-  "TELEGRAM_TOKEN": "YOUR_BOT_TOKEN",
-  "CHAT_ID": "YOUR_CHAT_ID",
-  "EXCHANGE": "binance"
-}
+# config.py
+import os
+
+SYMBOL = "BTC/USDT"
+TIMEFRAME = "15m"
+EXCHANGE = "binance"
+
+# خواندن از متغیرهای محیطی (که از Secrets می‌آیند)
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
