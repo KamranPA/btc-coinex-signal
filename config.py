c25a669c063f591f2db57179289324c9a446b368
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-16 14:01:13 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 # config.py
 import os
 
-SYMBOL = "BTC-USDT"
-TIMEFRAME = "15m"
-HIGHER_TIMEFRAME = "1h"  # برای فیلتر روند
+SYMBOL = "BTC-USDT"          # فرمت صحیح KuCoin
+TIMEFRAME = "1h"
 EXCHANGE = "kucoin"
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
