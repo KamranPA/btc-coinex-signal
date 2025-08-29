0264e293e7c7fc4c800a59cbdcd075abfb8f965c
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-29 11:24:05 +0330

    Update and rename config.py to src/indicators/ichimoku.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-import os
-
-SYMBOL = "BTC-USDT"          # فرمت صحیح KuCoin
-TIMEFRAME = "1h"
-EXCHANGE = "kucoin"
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
